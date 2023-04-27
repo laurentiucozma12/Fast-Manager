76b9c0b55ca49fc6064aeff2c3a7b6f2c859d9f5
Lau <laurentiucozma12@gmail.com>
2023-04-27 17:09:43 +0300

    register works :). P.S. add more conditions for password

--- classes/register.class.php
+++ classes/register.class.php
@@ -9,35 +9,36 @@ class Register extends Dbh {
         $options = array('cost'=>4);
         $hashedPassword = password_hash($password,PASSWORD_BCRYPT,$options);
         $stmt->bindParam(':password', $hashedPassword);
-        
-        if (!$stmt->execute(array($username, $email, $hashedPassword))) {
+    
+        if (!$stmt->execute()) {
             $stmt = null;
             header("location: ../index.php?error=stmtfailed");
             exit();
         }
-
+    
         $stmt = null;
     }
 
     protected function checkUser($username, $email) {
-        $stmt = $this->connect()->prepare('SELECT username FROM users WHERE username = :username OR email = :email;');
+        $stmt = $this->connect()->prepare('SELECT * FROM users WHERE username = :username OR email = :email;');
         $stmt->bindParam(':username', $username);
         $stmt->bindParam(':email', $email);
-
-        if (!$stmt->execute(array($username, $email))) {
+    
+        if (!$stmt->execute()) {
             $stmt = null;
             header("location: ../index.php?error=stmtfailed");
             exit();
         }
-
+    
         $resultCheck;
         if($stmt->rowCount() > 0) {
             $resultCheck = false;
         } else {
             $resultCheck = true;
         }
-
+    
         return $resultCheck;
     }
+    
 
 }
